935c39ace9c5aff23b1f4136309962cb2860f81a
Gabriele Dragotto <gabriele@dragotto.net>
2018-10-30 09:53:08 +0100

    1.5 Rotational tests

--- src/QuickTests.java
+++ src/QuickTests.java
@@ -10,40 +10,41 @@ public class QuickTests {
 	public static void main(String[] args) throws ErrorThrower, IloException, IOException, ContradictionException {
 
 		System.out.println("Quick Tests");
-		Partition prt = new Partition(29, 3);
-		ArrayList<Integer> tables = new ArrayList<Integer>();
-		tables.add(30);
-		for (int i = 0; i < 1; i++)
-			tables.add(3);
+		Partition prt = new Partition(45, 3);
+		ArrayList<ArrayList<Integer>> tables = new ArrayList<ArrayList<Integer>>();
+		tables = prt.loadPartition();
 		int cnt = 0, cntI = 0;
-		ArrayList<Integer> tcopy = new ArrayList<Integer>(tables);
-		Boolean Verbose = false;
-		Boolean ExportModels = false;
-		Boolean Choco = false;
-		Boolean TimeLimit = true;
-		int SolLimit = 1;
-		String Path = "";
-		DecimalFormat df = new DecimalFormat("0.0000");
-		TwoRotational instance = new TwoRotational(Verbose, SolLimit, ExportModels, Path, TimeLimit, Choco);
-		ArrayList<TwoRRotational_Solution> Solutions = instance.solve(tcopy);
-		if (Solutions.size() > 0) {
-			for (int i = 0; i < Solutions.size(); i++) {
-				System.out.println("Solution for " + Solutions.get(i).getOP_name());
-				System.out.println("\tStatus: " + Solutions.get(i).getStatus());
-				if (Solutions.get(i).getStatus().equals("Solved"))
-					cnt++;
-				else
-					cntI++;
-				System.out.println("\tLabellingTime: " + df.format(Solutions.get(i).getLabellingTime()));
-				System.out.println("\tNotes: " + Solutions.get(i).getNotes());
-				System.out.println("\tLabels.Size: " + Solutions.get(i).getLabels().length);
-				System.out.println("\tSolution: " + Solutions.get(i).getSolution());
+		for (int j = 0; j < tables.size(); j++) {
+			ArrayList<Integer> tcopy = new ArrayList<Integer>(tables.get(j));
+			Boolean Verbose = false;
+			Boolean ExportModels = false;
+			Boolean Choco = false;
+			Boolean TimeLimit = true;
+			int SolLimit = 1;
+			String Path = "";
+			DecimalFormat df = new DecimalFormat("0.0000");
+			TwoRotational instance = new TwoRotational(Verbose, SolLimit, ExportModels, Path, TimeLimit, Choco);
+			ArrayList<TwoRRotational_Solution> Solutions = instance.solve(tcopy);
+			if (Solutions.size() > 0) {
+				for (int i = 0; i < Solutions.size(); i++) {
+					System.out.println("Solution for " + Solutions.get(i).getOP_name());
+					System.out.println("\tStatus: " + Solutions.get(i).getStatus());
+					if (Solutions.get(i).getStatus().equals("Solved"))
+						cnt++;
+					else
+						cntI++;
+					System.out.println("\tLabellingTime: " + df.format(Solutions.get(i).getLabellingTime()));
+					System.out.println("\tNotes: " + Solutions.get(i).getNotes());
+					System.out.println("\tLabels.Size: " + Solutions.get(i).getLabels().length);
+					System.out.println("\tSolution: " + Solutions.get(i).getSolution());
 
+				}
+			} else {
+				System.out.println("No Solution found.");
+				cntI++;
 			}
-		} else {
-			System.out.println("No Solution found.");
-			cntI++;
 		}
+
 		System.out.print("Solved: " + cnt + "\tvs" + tables.size());
 		System.out.println("Encountered " + cntI + " fails");
 	}
